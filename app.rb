f11d6338a783e525fb7fd7ac3d95d51c543ec228
jotkafomat <k-j@xl.wp.pl>
2021-01-13 10:57:53 +0000

    add flash message informing about using repeated letter

--- app.rb
+++ app.rb
@@ -40,6 +40,9 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     @game.guess(letter)
+    if @game.inputs.include?(letter)
+      flash[:message] = "You have already used that letter."
+    end
     redirect '/show'
   end
 
